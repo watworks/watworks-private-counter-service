49d35e505aec5e6de36bf790780f5c2c92f881bc
Evan Villemez <evillemez@gmail.com>
2017-06-27 15:20:15 -0400

    updated app; added deps

--- src/app.php
+++ src/app.php
@@ -1,11 +1,40 @@
 <?php
 
+use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-$app = new \Silex\Application();
+// some checks, for testing
+if (!getenv("FOO")) {
+    throw new \RuntimeException("FOO env var required");
+}
+
+// setup app and error handling
+$app = new Application();
+$app->error(function(\Exception $e, $code) {
+    echo sprintf("App error: %s", $e->getMessage());
+});
 
 // TODO: define core services
+
+// index route to have *something* to look at
+$app->get("/", function() {
+    $str = <<<EOT
+    <html>
+        <body>
+            <h1>Hi.</h1>
+            <p>Test the example routes below.<p>
+            <ul>
+                <li><a href="/_health">/_health</a></li>
+                <li><a href="/hello">/hello</a></li>
+                <li><a href="/error">/error</a></li>
+            </ul>
+        <body>
+    <html>
+EOT;
+    return new Response($str);
+});
 
 // api routes
 $app->get("/_health", function() {
@@ -13,7 +42,11 @@ $app->get("/_health", function() {
 });
 
 $app->get("/hello", function() {
-    return new JsonResponse(['hello' => 'world']);
+    return new JsonResponse(['hello' => getenv("FOO")]);
+});
+
+$app->get("/error", function () {
+    throw new \RuntimeException("testing an error");
 });
 
 return $app;
