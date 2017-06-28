4185b4f65ae18fb2a5976c441ce56d6c48cb07c4
Evan Villemez <evillemez@gmail.com>
2017-06-28 08:42:34 -0400

    added api tests

--- tests/appTest.php
+++ tests/appTest.php
@@ -20,6 +20,18 @@ class Test extends TestCase
         return $app->handle($req);
     }
 
+    private function decodeJsonResponse(Response $res)
+    {
+        $data = $res->getContent();
+        return json_decode($data, true);
+    }
+
+    private function clearStorage()
+    {
+        $app = $this->loadApp();
+        $app['redis']->flushdb();
+    }
+
     public function testLoadApp()
     {
         $app = $this->loadApp();
@@ -42,8 +54,54 @@ class Test extends TestCase
     }
 
     // TODO: actually test the counter routes
-    public function testApiRoutes()
+    public function testCreateGetAndDeleteCounter()
     {
-        $this->markTestIncomplete();
+        $this->clearStorage();
+
+        // get a counter: shouldn't exist yet
+        $res = $this->callApi("GET", "/counters/foo");
+        $this->assertSame(404, $res->getStatusCode());
+
+        // create one, second call should be a no-op
+        $res = $this->callApi("PUT", "/counters/foo");
+        $this->assertSame(201, $res->getStatusCode());
+        $res = $this->callApi("PUT", "/counters/foo");
+        $this->assertSame(200, $res->getStatusCode());
+
+        // now retrieve it
+        $res = $this->callApi("GET", "/counters/foo");
+        $this->assertSame(200, $res->getStatusCode());
+        
+        // delete it
+        $res = $this->callApi("DELETE", "/counters/foo");
+        $this->assertSame(200, $res->getStatusCode());
+
+        // get it again - shouldn't exist
+        $res = $this->callApi("GET", "/counters/foo");
+        $this->assertSame(404, $res->getStatusCode());
+    }
+
+    public function testModifyCounter()
+    {
+        $this->clearStorage();
+
+        // create a counter - init at 100
+        $res = $this->callApi("PUT", "/counters/foo/100");
+        $this->assertSame(201, $res->getStatusCode());
+        $d = $this->decodeJsonResponse($res);
+        $this->assertSame(0, $d['prevValue']);
+        $this->assertSame(100, $d['value']);
+
+        // increment it
+        $this->callApi("PUT", "/counters/foo/increment/20");
+        $d = $this->decodeJsonResponse($res);
+        $this->assertSame(100, $d['prevValue']);
+        $this->assertSame(120, $d['value']);
+
+        // decrement(t)
+        $this->callApi("PUT", "/counters/foo/decrement/50");
+        $d = $this->decodeJsonResponse($res);
+        $this->assertSame(120, $d['prevValue']);
+        $this->assertSame(70, $d['value']);
     }
 }
